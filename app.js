427a0d8febe51bcff915a66587a2f2cb84c45e20
Maribel <minicatsCB@users.noreply.github.com>
2018-12-19 00:43:02 +0100

    Request forecast weather

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var pollutionUrl = "http://airemad.com/api/v1/pollution/";
 var cameraUrl = "http://informo.munimadrid.es/cameras/";
 var weatherApiKey = "yourApiKey";
-var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=";
+var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=";
+var forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Madrid&units=metric&&appid=";
 
 displayRandomCameraImage();
 
@@ -12,7 +13,8 @@ requestData(url)
         console.log("An error occurred while fetching pollution data: ", err);
     });
 
-requestWeather();
+requestCurrentWeather();
+requestForecastWeather();
 
 function replaceNullData(strings, ...parts) {
     var checkedMarkup = "";
@@ -63,12 +65,12 @@ function getRandomInteger(min, max) {
     return Math.floor(random);
 }
 
-function requestWeather(place, cb){
-    var url = weatherUrl + weatherApiKey;
+function requestCurrentWeather(place, cb){
+    var url = currentWeatherUrl + weatherApiKey;
     requestData(url)
         .then(displayCurrentWeahterData)
         .catch(err => {
-            console.log("An error occurred while fetching weather data: ", err);
+            console.log("An error occurred while fetching current weather data: ", err);
         });
 }
 
@@ -84,4 +86,68 @@ function displayCurrentWeahterData(data) {
     `
 
     currentWatherDataElement.innerHTML = currentWatherDataMarkup;
+}
+
+function requestForecastWeather() {
+    var url = forecastWeatherUrl + weatherApiKey;
+    requestData(url)
+        .then(data => {
+            console.log(data);
+            displayForecastWeatherData(data);
+        })
+        .catch(err => {
+            console.log("An error occurred while fetching forecast weather data: ", err);
+        });
+}
+
+function displayForecastWeatherData(data) {
+    var forecastElement = document.getElementsByClassName("forecast")[0];
+
+    var forecastMarkup = replaceNullData`
+        <div class="col">
+            <div class="col-12">${getWeekDayName(data.list[0].dt)} (00:00)</div>
+            <div class="row">
+                <div class="col-6">First day forecast icon</div>
+                <div class="col-6">First day forecast data</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-12">${getWeekDayName(data.list[8].dt)} (00:00)</div>
+            <div class="row">
+                <div class="col-6">First day forecast icon</div>
+                <div class="col-6">First day forecast data</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-12">${getWeekDayName(data.list[16].dt)} (00:00)</div>
+            <div class="row">
+                <div class="col-6">First day forecast icon</div>
+                <div class="col-6">First day forecast data</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-12">${getWeekDayName(data.list[24].dt)} (00:00)</div>
+            <div class="row">
+                <div class="col-6">First day forecast icon</div>
+                <div class="col-6">First day forecast data</div>
+            </div>
+        </div><div class="col">
+            <div class="col-12">${getWeekDayName(data.list[32].dt)} (00:00)</div>
+            <div class="row">
+                <div class="col-6">First day forecast icon</div>
+                <div class="col-6">First day forecast data</div>
+            </div>
+        </div>
+    `;
+
+    forecastElement.innerHTML = forecastMarkup;
+}
+
+function getWeekDayName(timestamp) {
+    var date = new Date(timestamp * 1000);
+    var options = {
+        weekday: 'long'
+    };
+
+    return date.toLocaleString("es-ES", options);
 }
