cecba0d694224d07cb9e41c44ee4af0f752fb933
Maribel <minicatsCB@users.noreply.github.com>
2019-01-22 17:16:06 +0100

    Move constants to a separate file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-var pollutionUrl = "http://airemad.com/api/v1/pollution/";
-var cameraUrl = "http://informo.munimadrid.es/cameras/";
-var weatherApiKey = "yourApiKey";
-var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=";
-var forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Madrid&units=metric&&appid=";
-
 displayRandomCameraImage();
 requestPollutionData();
 requestCurrentWeather();
