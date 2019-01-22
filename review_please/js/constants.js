f373773e05d0663e954e4cab102aaf81f888c213
Maribel <minicatsCB@users.noreply.github.com>
2019-01-22 17:37:24 +0100

    Enable pull request for reviewing

---
+++ review_please/js/constants.js
@@ -0,0 +1,5 @@
+const pollutionUrl = "http://airemad.com/api/v1/pollution/";
+const cameraUrl = "http://informo.munimadrid.es/cameras/";
+const weatherApiKey = "yourApiKey";
+const currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&lang=es&appid=";
+const forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Madrid&units=metric&lang=es&appid=";
