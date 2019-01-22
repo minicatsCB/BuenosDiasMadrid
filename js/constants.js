f18bf576cfba2acb8ec1b79e3e358182243899cc
Maribel <minicatsCB@users.noreply.github.com>
2019-01-22 17:21:49 +0100

    Add language parameter to URLs

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 var pollutionUrl = "http://airemad.com/api/v1/pollution/";
 var cameraUrl = "http://informo.munimadrid.es/cameras/";
 var weatherApiKey = "yourApiKey";
-var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=";
-var forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Madrid&units=metric&&appid=";
+var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&lang=es&appid=";
+var forecastWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Madrid&units=metric&lang=es&appid=";
